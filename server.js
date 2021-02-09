df00d0d449ad5543bfe9435c3345ea31b5703966
David Johnson <david.m.johnson32@gmail.com>
2021-02-08 16:07:24 -0800

    Fixed functions

--- server.js
+++ server.js
@@ -28,12 +28,12 @@ app.use(express.json());
 connection.connect(function (err) {
   if (err) throw err;
   console.log(`Connected as ID ${connection.threadId}`);
-  start();
+  startPrompt();
 });
 
 // Initial Execution
 
-function start() {
+function startPrompt() {
   inquirer
     .prompt([
       {
@@ -42,66 +42,62 @@ function start() {
         name: "choice",
         choices: [
           "View All Employees?",
-          "View All Employees By Roles?",
+          "View All Employee's By Roles?",
           "View all Emplyees By Deparments",
           "Update Employee",
           "Add Employee?",
           "Add Role?",
-          "Add Department?",
-        ],
-      },
-    ])
-    .then(function (val) {
-      switch (val.choices) {
-        case "View All Employees?":
-          viewAllEmployees();
-          break;
-
-        case "View All Employees By Roles?":
-          viewAllRoles();
-          break;
-
-        case "View All Employees By Department":
-          viewAllDepartments();
-          break;
-
-        case "Add Employee?":
-          addEmployee();
-          break;
-
-        case "Update Employee?":
-          updateEmployee();
-          break;
-
-        case "Add Role?":
-          addRole();
-          break;
-
-        case "Add Department?":
-          addDepartment();
-          break;
+          "Add Department?"
+        ]
       }
-    });
+    ])
+    .then(function(val) {
+      switch (val.choice) {
+          case "View All Employees?":
+            viewAllEmployees();
+          break;
+  
+        case "View All Employee's By Roles?":
+            viewAllRoles();
+          break;
+        case "View all Emplyees By Deparments":
+            viewAllDepartments();
+          break;
+        
+        case "Add Employee?":
+              addEmployee();
+            break;
+
+        case "Update Employee":
+              updateEmployee();
+            break;
+    
+          case "Add Role?":
+              addRole();
+            break;
+    
+          case "Add Department?":
+              addDepartment();
+            break;
+  
+          }
+  })
 }
-
-// All Employee function
-
+//============= View All Employees ==========================//
 function viewAllEmployees() {
-  connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+  connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
   function(err, res) {
     if (err) throw err
     console.table(res)
-    start()
-  })
+    startPrompt()
+})
 }
-
-// View Roles 
-
+//============= View All Roles ==========================//
 function viewAllRoles() {
-  connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
-  function(err, res) {
-  if (err) throw err
-  console.table(res)
-  start()
-  })
+connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+function(err, res) {
+if (err) throw err
+console.table(res)
+startPrompt()
+})
 }
